71afedfea966fd86551aa1cf6f334b71f142ffd6
Leonardo Souza <leonardo@pontualmt.com>
2020-03-16 05:37:45 -0700

    arvore

--- arvore.py
+++ arvore.py
@@ -1,11 +1,29 @@
 # -*- coding: utf-8 -*-
 from funcoes_arvore_decisao_entropia import partition_entropy_by, build_tree, classify
+def create_inputs():
+    inputs = [ 
+        ({'level':'Senior','lang':'Java','tweets':'no','phd':'no'},   False), 
+        ({'level':'Senior','lang':'Java','tweets':'no','phd':'yes'},  False), 
+        ({'level':'Mid','lang':'Python','tweets':'no','phd':'no'},     True), 
+        ({'level':'Junior','lang':'Python','tweets':'no','phd':'no'},  True), 
+        ({'level':'Junior','lang':'R','tweets':'yes','phd':'no'},      True), 
+        ({'level':'Junior','lang':'R','tweets':'yes','phd':'yes'},    False), 
+        ({'level':'Mid','lang':'R','tweets':'yes','phd':'yes'},        True), 
+        ({'level':'Senior','lang':'Python','tweets':'no','phd':'no'}, False), 
+        ({'level':'Senior','lang':'R','tweets':'yes','phd':'no'},      True), 
+        ({'level':'Junior','lang':'Python','tweets':'yes','phd':'no'}, True), 
+         ({'level':'Senior','lang':'Python','tweets':'yes','phd':'yes'},True), 
+        ({'level':'Mid','lang':'Python','tweets':'no','phd':'yes'},    True), 
+        ({'level':'Mid','lang':'Java','tweets':'yes','phd':'no'},      True), 
+        ({'level':'Junior','lang':'Python','tweets':'no','phd':'yes'},False) 
+    ] 
+    return inputs
+
+
 
 if __name__ == "__main__":
 
-    inputs = [({'level':'Senior','lang':'Java','tweets':'no','phd':'no'},False),
-              ({'level':'Junior','lang':'.NET','tweets':'yes','phd':'yes'},True)  
-             ]
+    inputs = create_inputs()
     
     for key in ['level','lang','tweets','phd']:
         print key, partition_entropy_by(inputs,key)
